9ec36010269d1c14eb0396c0fb36b78c8558ab32
Руслан <newroosh90@gmail.com>
2014-06-20 15:41:32 +0600

    Достиг цели обратной необходимой убиваю все фичи при увеличении масштаба, но не могу перерисовать их при уменьшении надо ковырять BBOX;

--- Hnome-testtask2014-aec34317d9ed/client.js
+++ Hnome-testtask2014-aec34317d9ed/client.js
@@ -16,12 +16,46 @@ function onload(){
             format: new OpenLayers.Format.GeoJSON({
             })
         }),
+//        eventListener : {
+//            'updatesize' : clearBuildingAfterResize,
+//            'zoomend': clearBuilddingAfrerZoomend
+//        }
     });
 
+
     map.addLayer(building);
+
+
 
     map.setCenter(
         new OpenLayers.LonLat(60.6069, 56.8370).transform("EPSG:4326", "EPSG:900913"),
         15
     );
+
+    map.events.register('zoomend', map, function(){
+        building.destroyFeatures();
+        console.log('zoomend')}
+    );
+//    map.events.register('updatesize', map, function(){console.log('updatesize')});
+//    map.events.register("move", map, function() {console.log("panning");});
+    map.events.register('zoomstart', map, function(){console.log('zoomstart')});
+//
+//
+//    building.events.register("refresh", building, function(){console.log('refresh')});
+//    building.events.register('move', building, function(){console.log('move')});
+//    building.events.register('moveend', building, function(){
+//            console.log('moveend');
+//        }
+//    );
+//
+//    building.events.register('loadstart', building, function(){console.log('loadstart')});
+//    building.events.register('loadend', building, function(){console.log('loadend');});
+//
+//    building.events.register('sketchstarted', building, function(){console.log('sketchstarted');});
+//    building.events.register('sketchmodified', building, function(){console.log('sketchmodified');});
+//    building.events.register('sketchcomplete', building, function(){console.log('sketchcomplete');});
+
+//    map.events.register()
+
+//    map.events.register('moveend', map, function(){clearAfterMove(map, building)});
 }
